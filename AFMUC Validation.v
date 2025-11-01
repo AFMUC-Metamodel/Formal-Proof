2b2f8f5ac75874a3a4eb5caa80592429a72e9156
Shumaila-iq <shumaila_iqbal99@yahoo.com>
2025-11-01 20:09:31 +0500

    Update AFMUC Validation.v

--- AFMUC Validation.v
+++ AFMUC Validation.v
@@ -5,8 +5,8 @@ Require Import Arith.
 Open Scope string_scope.
 Require Import Coq.Strings.String.
 Require Import Coq.Bool.Bool.
-Load "C:\Users\shumaila.iqbal\OneDrive - Riphah International University\Documents\Ph.D research work\3. PhD dissertation\2. Metamodel\AFMUC_Definitions.v".
-Load "C:\Users\shumaila.iqbal\OneDrive - Riphah International University\Documents\Ph.D research work\3. PhD dissertation\2. Metamodel\AFMUC_Instances.v".
+(*Load "reference location of AFMUC_Definitions.v".*)
+(*Load "reference location of AFMUC_Instances.v".*)
 
 
 
